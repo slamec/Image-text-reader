31e44375fceb4becf5915c89d5db43bda340fdec
Miroslav Kopecky <kopecky.mir@gmail.com>
2021-01-03 16:57:33 +0100

    basic file working

--- Image_to_text_basic.py
+++ Image_to_text_basic.py
@@ -1,11 +1,11 @@
-def ImageToText():
-    import cv2
-    import pytesseract #possible to use also PDF and so on - click one pytesseract
-    
-    pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-    image = cv2.imread('test.png')
+import cv2
+import pytesseract #possible to use also PDF and so on - click one pytesseract
     
-    st = pytesseract.image_to_string(image)
+pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-    print(st)
+image = cv2.imread('test.png')
+    
+st = pytesseract.image_to_string(image)
+
+print(st)
