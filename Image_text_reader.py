987d436395ba8bb631e0040dba8c3acbce62638b
Miroslav Kopecky <kopecky.mir@gmail.com>
2021-01-04 12:25:01 +0100

    almost working

--- Image_text_reader.py
+++ Image_text_reader.py
@@ -1,6 +1,9 @@
 from tkinter import filedialog
 from tkinter import * #import all function from tkinter
 import sys
+import getpass
+# Need this for the `os.path.split` function
+import os
 root = Tk("Miro")
 
 if sys.version_info[0] < 3:
@@ -9,24 +12,32 @@ else:
    import tkinter as Tk
 
 
-def browse_button():
-    filename = filedialog.askopenfile()
+def Browse_button():
+    global directory 
+    # Get the file
+    file = filedialog.askopenfilename(initialdir='C:/Users/%s')
+    # Split the filepath to get the directory
+    directory = os.path.split(file)[0]
+    print(directory)
 
 def ImageToText():
     import cv2
     import pytesseract #possible to use also PDF and so on - click one pytesseract
     
     pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-
-    image = cv2.imread(browse_button) 
+    
+    image = cv2.imread(directory)
 
     st = pytesseract.image_to_string(image)
 
     print(st)
 
-root.configure(background = 'light green')
-root.geometry("400x175")
-v = StringVar()
-button1 = Button(text = "Image Location :", command=browse_button).grid(row=0, column=3)
-button2 = Button(text = "Convert", command =ImageToText).grid(row=0, column=4)
-mainloop() 
+#GUI driver
+if __name__ == "__main__":
+
+    root.configure(background = 'light green')
+    root.geometry("400x175")
+    v = StringVar()
+    button1 = Button(text = "Image Location :", command = Browse_button).grid(row=0, column=3)
+    button2 = Button(text = "Convert", command = ImageToText).grid(row=0, column=4)
+    mainloop() 
--- Image_to_text_basic.py
+++ Image_to_text_basic.py
@@ -4,7 +4,7 @@ import pytesseract #possible to use also PDF and so on - click one pytesseract
     
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-image = cv2.imread('test.png')
+image = cv2.imread(input("Please insert file path or name :"))
     
 st = pytesseract.image_to_string(image)
 
