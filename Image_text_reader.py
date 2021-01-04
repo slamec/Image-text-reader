39e61893de04e8458de3d5713e3650e2418bad54
Miroslav Kopecky <kopecky.mir@gmail.com>
2021-01-04 19:21:38 +0100

    saving file as

--- Image_text_reader.py
+++ Image_text_reader.py
@@ -5,12 +5,13 @@ import getpass
 # Need this for the `os.path.split` function
 import os
 root = Tk()
-root.title("Image to text converte by Miro 2021") #title of main window
+root.title("Image to text converter by Miro 2021") #title of main window
 
 if sys.version_info[0] < 3:
    import Tkinter as Tk
 else:
    import tkinter as Tk
+
 
 
 def Browse_button():
@@ -32,7 +33,17 @@ def ImageToText():
 
     st = pytesseract.image_to_string(image)
 
-    print(st)
+    print(st)    
+
+def Save_file_as():
+
+    f = filedialog.asksaveasfile(mode='w', defaultextension=".txt")
+    if f is None: # asksaveasfile return `None` if dialog closed with "cancel".
+        return
+    text2save = st # starts from `1.0`, not `0.0`
+    f.write(text2save)
+    f.close() # `()` was missing.
+
 
 #GUI driver
 if __name__ == "__main__":
@@ -40,6 +51,9 @@ if __name__ == "__main__":
     root.configure(background = 'light green')
     root.geometry("400x175")
     v = StringVar()
-    button1 = Button(text = "Image Location :", command = Browse_button).grid(row=0, column=3)
+    button1 = Button(text = "Image Location: ", command = Browse_button).grid(row=0, column=3)
     button2 = Button(text = "Convert", command = ImageToText).grid(row=0, column=4)
+    button3 = Button(text = "Save file as: ", command = Save_file_as,).grid(row=0, column=5)
     mainloop() 
+
+#take st and save it as text file
